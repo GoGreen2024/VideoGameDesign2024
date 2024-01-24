ef82982a059e5c85c68f2bc852af3d970aae6994
CatherineLi5 <98063395+CatherineLi0531@users.noreply.github.com>
2024-01-24 17:12:17 -0600

    Instructions

--- code.js
+++ code.js
@@ -30,4 +30,12 @@ function yay(){
     alert("You managed to crack the riddles; the Queen has been saved. You have been awared a medal of honor.");
 }
 
+function instructions(){
+    alert("The beloved queen has fallen ill. Brew potions to earn money to go on quests (Witch's Hat Button). Each quest costs 100 Gold Coins. On each quest, you will solve a riddle that will bring you colser to eventually unlocking the cure room that contains the ingredients and instructions to cure the queen. Good Luck!");
+}
+
+function directions(){
+    alert("You've unlocked the cure room, in it you will find all you need to cure the queen. Be careful, one you add an ingredient it cannont be removed. To feed your remedy by pressing the flask button in the bottm left. Read Carefully!");
+}
+
 
