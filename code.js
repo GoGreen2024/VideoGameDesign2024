f4b389caaff30e14f1d7aa92b25d1b4302203cf8
CatherineLi5 <98063395+CatherineLi0531@users.noreply.github.com>
2024-01-24 16:56:41 -0600

    Code

---
+++ code.js
@@ -0,0 +1,33 @@
+function instructions(){
+    alert("Instructions Stuff");
+}
+
+function startGame(){
+    window.open("storefront.html", "_self");
+}
+
+function exit(){
+    window.open("Exit.html", "_self");
+}
+
+function mail(){
+    window.open("mail.html", "_self");
+}
+
+function quest(){
+    window.open("quest.html", "_self");
+}
+
+function cure(){
+    window.open("cure.html", "_self");
+}
+
+function death(){
+    alert("Your potion was incorrect; the Queen has passed.");
+}
+
+function yay(){
+    alert("You managed to crack the riddles; the Queen has been saved. You have been awared a medal of honor.");
+}
+
+
